6749ec1163b2f5a653daac0d937ae40c4683ee92
Krishnaba79 <solankikrishnabahitendrasinh@gmail.com>
2025-05-03 01:11:37 +0530

    Add files via upload

---
+++ arrayExercise1.swift
@@ -0,0 +1,24 @@
+// Practice Questions
+// Create an array called fruits with values: "Apple", "Banana", "Mango".
+
+// Add "Orange" to the array.
+
+// Remove "Banana" using its index.
+
+// Insert "Grapes" at index 1.
+
+// Print the number of fruits in the array.
+
+// Print the first and last fruit in the array.
+
+var fruits=["Apple","Banana","Mango"]
+
+fruits.append("Orange")
+
+fruits.insert("Grapes",at:1)
+
+print(fruits.count)
+
+print(fruits.first!)
+
+print(fruits.last!)
