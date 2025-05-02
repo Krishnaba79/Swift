9557ff374b55b0256d8a4f0d62a388cab2febf2f
Krishnaba79 <solankikrishnabahitendrasinh@gmail.com>
2025-05-03 01:02:23 +0530

    Add files via upload

---
+++ numberArrayExample.swift
@@ -0,0 +1,14 @@
+//Declare an Array of numbers.
+var numbers=[10,20,30]
+
+//Add a number
+numbers.append(40)
+
+//Remove a number
+numbers.remove(at: 1) //removes 20
+
+// Insert a number at index 1
+numbers.insert(25,at:1)
+
+//print final array
+print(numbers)
