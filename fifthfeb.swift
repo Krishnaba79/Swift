2561f57779cd84e717f2d5acd3d7fc31ea8b756b
Krishnaba79 <solankikrishnabahitendrasinh@gmail.com>
2025-05-03 01:16:04 +0530

    Update fifthfeb.swift

--- fifthfeb.swift
+++ fifthfeb.swift
@@ -1,8 +1,17 @@
+// This is a structure used to store data about a game character.
 struct GameCharacter{
     var name :String
     var health : Int
     var powerLevel:Int
 }
+
+// name is a String
+
+// health is an Int (e.g., 100)
+
+// powerLevel is also an Int (e.g., 1)
+
+//This is a class representing the game itself.
 public class Game{
     //let shared=Game()
 
@@ -15,6 +24,14 @@ public class Game{
     
 
 }
+
+// startGame() is a function that just prints a message.
+
+// This function doesn’t take any input and doesn’t return anything.
+
+
+
+//This is a function outside the class that creates and returns a GameCharacter.
 
 func createCharacter (name:String)-> GameCharacter{
     let newCharacter = GameCharacter (name:name,health:100,powerLevel:1)
